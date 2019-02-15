17a1238b4fcbb051bbff57645e095ce5ddaf9a1e
OlhaKryvych <olgakrivich@gmail.com>
2019-02-15 23:20:20 +0200

    Add output "Epam"

---
+++ Homework_1_Java/src/com/epam/homework1/Main.java
@@ -0,0 +1,14 @@
+package com.epam.homework1;
+
+public class Main
+{
+
+    public static void main(String[] args)
+    {
+        System.out.println("* * * *   * * * *       *        *     *");
+        System.out.println("*         *     *      * *       * * * *");
+        System.out.println("* *       * * * *     *   *      *  *  *");
+        System.out.println("*         *          * * * *     *     *");
+        System.out.println("* * * *   *         *       *    *     *");
+    }
+}
